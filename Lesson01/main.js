943313bf1e4b9947c7e01bba6b4432d4b9bcea08
Toras <efpan@mail.ru>
2021-03-23 18:24:42 +0700

    hw for lesson 1

--- Lesson01/main.js
+++ Lesson01/main.js
@@ -1,5 +1,4 @@
-function getTc()
-{
+function getTc() {
     let Tc = +document.getElementsByName('Tc')[0].value;
     let Tf = (9 / 5) * Tc + 32;
     document.getElementsByName('result1')[0].value = 'Tf=(9/5)*Tc+32=' + Tf + '\n'
@@ -8,7 +7,3 @@ function getTc()
 let name = 'Василий';
 let admin = name;
 document.getElementsByName('result2')[0].value += admin;
-
-//alert(admin);
-
-//alert(1000 + '108')
